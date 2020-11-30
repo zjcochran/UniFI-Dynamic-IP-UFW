d73a237e8c15bb18a14764fc511fa2b1d4ca5e80
Zach <66173351+zjcochran@users.noreply.github.com>
2020-11-30 13:38:16 -0500

    Add basic logging

--- UFW-DynIP.py
+++ UFW-DynIP.py
@@ -1,11 +1,18 @@
 import pyufw as ufw
 import socket
+import logging
+
+logging.basicConfig(
+        filename="/var/log/UFW-DynIP.log",
+        level=logging.INFO,
+        format="%(asctime)s:%(levelname)s:%(message)s"
+        )
 
 # You only need to modify the hosts variable to fit your needs.
 # You could add more ports if you want, but what is here is all that you need.
 # Adding port 9090 is optional if you followed the online guides and installed cockpit
 hosts = [""]
-ports = ["2222","8443"]
+ports = ["2222", "8443"]
 
 # Needed empty array
 addresses = []
@@ -25,8 +32,10 @@ for address in addresses:
     for rule in rules:
         if address in rules[rule]:
             ufw.delete(rules[rule])
-    
+            logging.info("Deleted rule {}".format(rules[rule]))
+
     # And add in the new rules
     for port in ports:
         new_rule = "allow from {} to any port {}".format(address, port)
         ufw.add(new_rule)
+        logging.info("Added rule {}".format(new_rule))
