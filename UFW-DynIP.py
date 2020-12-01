e235b39590f92765677a2b11db98ac65248c1bba
zjcochran <zjcochran@gmail.com>
2020-11-30 21:23:01 -0500

    Adding error checking

--- UFW-DynIP.py
+++ UFW-DynIP.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
+import socket, logging, sys
 import pyufw as ufw
-import socket
-import logging
 
 logging.basicConfig(
         filename="/var/log/UFW-DynIP.log",
@@ -25,19 +24,29 @@ rules = status["rules"]
 
 # Get the dynamic addresses from the hosts list
 for host in hosts:
-    ip = socket.gethostbyname(host)
-    addresses.append(ip)
+    try:
+        ip = socket.gethostbyname(host)
+        addresses.append(ip)
+    except:
+        logging.error("Error looking up host {}".format(host))
+        sys.exit()
 
 # Main loop
 for address in addresses:
     # Delete the old rules first
     for rule in rules:
         if address in rules[rule]:
-            ufw.delete(rules[rule])
-            logging.info("Deleted rule {}".format(rules[rule]))
+            try:
+                ufw.delete(rules[rule])
+                logging.info("Deleted rule {}".format(rules[rule]))
+            except:
+                logging.error("Error deleting rule {}".format(rules[rule]))
 
     # And add in the new rules
     for port in ports:
-        new_rule = "allow from {} to any port {}".format(address, port)
-        ufw.add(new_rule)
-        logging.info("Added rule {}".format(new_rule))
+        try:
+            new_rule = "allow from {} to any port {}".format(address, port)
+            ufw.add(new_rule)
+            logging.info("Added rule {}".format(new_rule))
+        except:
+            logging.error("Error creating rule {}".format(rules[rule]))
