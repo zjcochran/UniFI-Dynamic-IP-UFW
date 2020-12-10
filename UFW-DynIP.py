d02ebd3fb3c856144b1dce43df37f02dc36f00b1
zjcochran <zjcochran@gmail.com>
2020-12-09 19:49:28 -0500

    Adding a config file option to track old addresses and redid the way previous rules were removed - realized it would keep the old IP address rules in place if the dynamic ip address changed.  This is just a quick recode to get it working, a full recode to optimize will come next.

--- UFW-DynIP.py
+++ UFW-DynIP.py
@@ -20,11 +20,12 @@ logging.basicConfig(
 # You only need to modify the hosts variable to fit your needs.
 # You could add more ports if you want, but what is here is all that you need.
 # Adding port 9090 is optional if you followed the online guides and installed cockpit
-hosts = config.get('main_config', 'hosts').split()
-ports = config.get('main_config', 'ports').split()
+hosts = config.get('main_config', 'hosts').split(',')
+ports = config.get('main_config', 'ports').split(',')
+prev_addresses = config.get('main_config', 'prev_addresses')
 
 # Needed empty array
-addresses = []
+new_addresses = []
 
 # Get the list of rules
 status = ufw.status()
@@ -33,14 +34,30 @@ rules = status["rules"]
 # Get the dynamic addresses from the hosts list
 for host in hosts:
     try:
-        addresses.append(socket.gethostbyname(host))
+        new_addresses.append(socket.gethostbyname(host))
     except:
         logging.error("Error looking up host {}".format(host))
         sys.exit()
 
-# Main loop
-for address in addresses:
-    # Delete the old rules first
+# Check for & delete the old rules first
+if prev_addresses != '':
+    old_addresses = prev_addresses.split(',')
+    for address in old_addresses:
+        for rule in rules:
+            if address in rules[rule]:
+                try:
+                    ufw.delete(rules[rule])
+                    logging.info("Deleted rule {}".format(rules[rule]))
+                except:
+                    logging.error("Error deleting rule {}".format(rules[rule]))
+
+# Refresh the list of rules
+status = ufw.status()
+rules = status["rules"]
+
+# Add in the new rules
+for address in new_addresses:
+    # Double check for existing rules
     for rule in rules:
         if address in rules[rule]:
             try:
@@ -49,7 +66,7 @@ for address in addresses:
             except:
                 logging.error("Error deleting rule {}".format(rules[rule]))
 
-    # And add in the new rules
+    # Create the new rules
     for port in ports:
         new_rule = "allow from {} to any port {}".format(address, port)
         try:
@@ -57,3 +74,8 @@ for address in addresses:
             logging.info("Added rule {}".format(new_rule))
         except:
             logging.error("Error creating rule {}".format(new_rule))
+
+config['main_config']['prev_addresses'] = ",".join(new_addresses)
+
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
